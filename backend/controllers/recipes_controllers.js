55134c54e87ca973de940a4b1bda9957e8b13377
wguadalupe <w.guadalupe78@gmail.com>
2024-03-28 13:25:35 -0400

    Updated routes and added an update route

--- backend/controllers/recipes_controllers.js
+++ backend/controllers/recipes_controllers.js
@@ -15,4 +15,20 @@ router.post('/', async (req, res) => {
     res.status(201).send(newRecipe); 
 });
 
+//Update
+
+router.put('/:id', async (req, res) => {
+    try {
+      const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedRecipe) {
+        return res.status(404).send('Recipe not found.');
+      }
+      // Assuming you want to return the updated recipe to the client
+      res.json(updatedRecipe);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while updating the recipe.');
+    }
+  });
+  
 module.exports = router;
