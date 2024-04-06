bc328b4a1c10392b8532b38f025abfb50066d036
Doidae <andyarredondo1224@gmail.com>
2024-04-06 15:00:45 -0400

    made server.js and connected it to database

---
+++ server.js
@@ -0,0 +1,33 @@
+const methodOverride = require('method-override')
+const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(
+    () => console.log('connected to the database!~')
+    )
+
+app.get('/', (req,res) =>{
+    res.send('Welcome to MY recipe app')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT)
+})
+
+//ERROR PAGE
+app.get('*', (req,res) => {
+    res.send('404')
+})
