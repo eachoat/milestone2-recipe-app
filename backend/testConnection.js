9284519c097f5834b300477ff47c68ee997ff0a4
wguadalupe <w.guadalupe78@gmail.com>
2024-04-06 14:06:06 -0400

    Delete backend directory
    Duplicate

--- backend/testConnection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const uri = "mongodb://localhost:27017/recipe";
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected successfully to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB', err));
