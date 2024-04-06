839793642e44a91ebb00e89c7cd43ad97db53db2
Doidae <andyarredondo1224@gmail.com>
2024-04-06 15:45:30 -0400

    fixed error with index

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-// app.engine('jsx', require('express-react-views').createEngine())
+app.engine('jsx', require('express-react-views').createEngine())
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(
     () => console.log('connected to the database!~')
